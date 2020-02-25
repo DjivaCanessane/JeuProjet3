658034f25935befd666973c3f1f84bd3fda8890d
Djiva Canessane <dcanessane@icloud.com>
2020-02-25 23:19:49 +0100

    warrior model added

--- JeuProjet3/Models/Warrior.swift
+++ JeuProjet3/Models/Warrior.swift
@@ -1,8 +1,8 @@
 //
 //  Warrior.swift
-//  Jeu
+//  JeuProjet3
 //
-//  Created by Djiva Canessane on 17/01/2020.
+//  Created by Djiva Canessane on 25/02/2020.
 //  Copyright © 2020 Djiva Canessane. All rights reserved.
 //
 
