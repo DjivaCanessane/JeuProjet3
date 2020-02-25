f9a946eef9be24c98c2044b7640f1ee915603cc5
Djiva Canessane <dcanessane@icloud.com>
2020-02-25 23:18:32 +0100

    Weapon and WeaponType model added

--- JeuProjet3/Models/WeaponType.swift
+++ JeuProjet3/Models/WeaponType.swift
@@ -1,8 +1,8 @@
 //
 //  WeaponType.swift
-//  Jeu
+//  JeuProjet3
 //
-//  Created by Djiva Canessane on 24/01/2020.
+//  Created by Djiva Canessane on 25/02/2020.
 //  Copyright © 2020 Djiva Canessane. All rights reserved.
 //
 
